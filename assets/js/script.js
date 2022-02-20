083c1cd1c93f5ffb105b21e97eea37f359ac893b
Travis Nelson <travislnelson92@gmail.com>
2022-02-19 22:31:21 -0800

    achieved basic funcionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
 var generateBtn = document.querySelector("#generate");
 
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-
 var collectArray = [
   [randomLow()], 
   [randomUpper()], 
@@ -24,48 +22,54 @@ function writePassword() {
       var passwordLength = lengthPrompt();
 
         // for function to print password 
-      for (var i = lengthPrompt.length; i < length; i ++) {
-        generatedPassword += passwordTypes[Math.floor(Math.random() * lengthPrompt.length)];
+      for (var i = 0; i < passwordLength; i ++) {
+        generatedPassword += passwordTypes[Math.floor(Math.random() * passwordTypes.length)];
       }
     }
     return generatedPassword; 
   };
 };
 
-function collectDataTypes() {
+function collectDataTypes(dynamicArray, confChar) {
   // prompt users for included character type
   var lowerConf = window.confirm("Include lowercase characters? Press OK for yes, and CANCEL for no.");
-  while(lowerConf === true) {
-    randomLow() 
-    collectArray.push[randomLow()];
+  // check if yes
+  if(lowerConf === true) {
+    randomLow(confChar.push(...dynamicArray[0].slice(0)));
+  } else {
+    return confChar.splice(0, confChar.length);
   }
  
   var upperConf = window.confirm("Include uppercase characters? Press OK for yes, and CANCEL for no. ");
   console.log(upperConf);
-  while(upperConf === true) {
-    randomUpper()  
-    collectArray.push[randomUpper()];
-  }
+  if(upperConf === true) {
+    randomUpper(confChar.push(...dynamicArray[1].slice(0)));
+  } else {
+    return confChar.splice(0, confChar.length);
+  };
   
   var numericConf = window.confirm("Include numbers in your password? Press OK for yes, and CANCEL for no.");
   console.log(numericConf);
-  while(numericConf === true) {
-    randomNumber();
-    collectArray.push[randomNumber()];
-  }
+  if(numericConf === true) {
+    randomNumber(confChar.push(...dynamicArray[2].slice(0)));
+  } else {
+    return confChar.splice(0, confChar.length);
+  };
 
   var specialConf = window.confirm("Include special characters? Press OK for yes, and CANCEL for no. ");
   console.log(specialConf);
-  while(specialConf === true) {
-    randomSpecial() 
-    collectArray.push[randomSpecial()];
-  }
-} 
+  if(specialConf === true) {
+    randomSpecial(confChar.push(...dynamicArray[3].slice(0)));
+  } else {
+    return confChar.splice(0, confChar.length);
+  };
+
+};
     
     
 function lengthPrompt() {
   // prompt user for desired length
-  passwordLength = window.prompt("How long would you like your password? Min 8 characters, Max 128 characters. ");
+  passwordLength = prompt("How long would you like your password? Min 8 characters, Max 128 characters. ");
   // if outside length parameters, alert to reinput
   if(passwordLength !==null){
     parseInt(passwordLength)
@@ -73,14 +77,14 @@ function lengthPrompt() {
       window.alert("Invalid length input, please input a number between 8-128.");
       lengthPrompt();
     }
+    // rerun password length prompt if falsy value input
     return passwordLength;
   } else {
     return;
   }
 };
 
-
-// charactar randomizer functions
+// character randomizer functions
 
 function randomLow() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -95,7 +99,7 @@ function randomNumber() {
 };
 
 function randomSpecial() {
-  return String.symbols[Math.floor(Math.random() * symbols.length)];
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
 };
 
 
