c28b2ce9a14a21299afb14c05b0280fc57cc645f
Travis Nelson <travislnelson92@gmail.com>
2022-02-20 03:01:20 -0800

    resolving password output function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,29 @@
 var generateBtn = document.querySelector("#generate");
 
-var collectArray = [
-  [randomLow()], 
-  [randomUpper()], 
-  [randomNumber()], 
-  [randomSpecial()]
-];
+// character code randomizer functions
+function randomLow() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+  
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+function randomSpecial() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+};
+  
+const collectObj = {
+  lower: randomLow(),
+  upper: randomUpper(),
+  number: randomNumber(),
+  special: randomSpecial()
+};
+
 
 // Write password to the #password input
 function writePassword() {
@@ -14,54 +32,61 @@ function writePassword() {
   passwordText.value = password;
 
   function generatePassword() {
-    var passwordTypes = [];
+    var passwordTypes = Object.keys(0, 1, 2, 3);
     var generatedPassword = "";
 
-    collectDataTypes(collectArray, passwordTypes);
+    collectDataTypes(collectObj, passwordTypes);
     if (passwordTypes.length !== 0){
       var passwordLength = lengthPrompt();
 
         // for function to print password 
-      for (var i = 0; i < passwordLength; i ++) {
+      for (let i = 0; i < passwordLength; i++) {
+        
         generatedPassword += passwordTypes[Math.floor(Math.random() * passwordTypes.length)];
+        //  lower, upper, number, special += generatedPassword += passwordTypes;
       }
     }
+    console.log(generatedPassword);
     return generatedPassword; 
   };
 };
 
-function collectDataTypes(dynamicArray, confChar) {
+function collectDataTypes(collectObj, passwordTypes) {
   // prompt users for included character type
-  var lowerConf = window.confirm("Include lowercase characters? Press OK for yes, and CANCEL for no.");
+  lowerConf = window.confirm("Include lowercase characters? Press OK for yes, and CANCEL for no.");
+  console.log(lowerConf);
   // check if yes
   if(lowerConf === true) {
-    randomLow(confChar.push(...dynamicArray[0].slice(0)));
-  } else {
-    return confChar.splice(0, confChar.length);
+    lower = randomLow();
+    randomLow(passwordTypes.push(...collectObj + 1));
   }
  
-  var upperConf = window.confirm("Include uppercase characters? Press OK for yes, and CANCEL for no. ");
+  upperConf = window.confirm("Include uppercase characters? Press OK for yes, and CANCEL for no. ");
   console.log(upperConf);
   if(upperConf === true) {
-    randomUpper(confChar.push(...dynamicArray[1].slice(0)));
-  } else {
-    return confChar.splice(0, confChar.length);
+    upper = randomUpper();
+    randomUpper(passwordTypes.push(...collectObj + 1));
   };
   
-  var numericConf = window.confirm("Include numbers in your password? Press OK for yes, and CANCEL for no.");
+  numericConf = window.confirm("Include numbers in your password? Press OK for yes, and CANCEL for no.");
   console.log(numericConf);
   if(numericConf === true) {
-    randomNumber(confChar.push(...dynamicArray[2].slice(0)));
-  } else {
-    return confChar.splice(0, confChar.length);
+    number = randomNumber();
+    randomNumber(passwordTypes.push(...collectObj + 1));
   };
 
-  var specialConf = window.confirm("Include special characters? Press OK for yes, and CANCEL for no. ");
+  specialConf = window.confirm("Include special characters? Press OK for yes, and CANCEL for no. ");
   console.log(specialConf);
   if(specialConf === true) {
-    randomSpecial(confChar.push(...dynamicArray[3].slice(0)));
-  } else {
-    return confChar.splice(0, confChar.length);
+    special = randomSpecial();
+    randomSpecial(passwordTypes.push(...collectObj + 1));
+  };
+
+  // if array empty, recall collectDataTypes function
+  if(passwordTypes.length === 0){
+    window.alert("You must enter atleast one valid selection");
+    return collectDataTypes(collectObj, passwordTypes);
+    
   };
 
 };
@@ -78,31 +103,14 @@ function lengthPrompt() {
       lengthPrompt();
     }
     // rerun password length prompt if falsy value input
+    console.log(passwordLength);
     return passwordLength;
   } else {
     return;
   }
 };
 
-// character randomizer functions
-
-function randomLow() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
-
-function randomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-};
-
-function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-};
-
-function randomSpecial() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
-};
-
-
+console.log(collectObj);
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
